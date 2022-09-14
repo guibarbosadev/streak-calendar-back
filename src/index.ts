8144045bed5f3f8111844470a1976ad10938fc3b
Guilherme Barbosa <guilherme.barbosa@kyte.com.br>
2022-09-14 20:25:28 -0300

    feat: set default headers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { PORT } from "./constants";
+import helmet from "helmet";
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+
 app.get("*", (_request, response) => {
   response.json({ up: "and running" });
 });
