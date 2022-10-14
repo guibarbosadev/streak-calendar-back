8734a8924c5e233781a6bc8754bcefe25224eee0
Guilherme Barbosa <me@guibarbosa.dev>
2022-10-14 19:02:44 -0300

    feat: set 8080 as default port

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = Number(process.env.PORT);
+export const PORT = Number(process.env.PORT) || 8080;
 export const DB_URI = process.env.DB_URI;
 export const DB_NAME = process.env.DB_NAME;
