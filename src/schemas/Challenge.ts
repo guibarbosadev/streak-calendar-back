4e2a0827080534e2bfec9be6fe77512bc1691d9e
Guilherme Barbosa <me@guibarbosa.dev>
2022-12-18 14:09:06 -0300

    feat: create ChallengeSchema

---
+++ src/schemas/Challenge.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+export enum EChallengeStatus {
+  Done = "done",
+  Skipped = "skipped",
+}
+
+export interface IChallengeCalendar {
+  [year: number]: {
+    [month: number]: {
+      [day: number]: EChallengeStatus;
+    };
+  };
+}
+
+interface IChallenge {
+  name: string;
+  _id: string;
+  calendar: IChallengeCalendar;
+}
+
+const ChallengeSchema = new Schema<IChallenge>({
+  _id: Types.ObjectId,
+  name: { type: String, required: true },
+  calendar: { type: Map, of: Object },
+});
+
+export const Challenge = mongoose.model<IChallenge>(
+  "challenges",
+  ChallengeSchema,
+  "users.challenges"
+);
